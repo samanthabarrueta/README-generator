c2ea91a47c5b6be52acc0c3f65d8d0c349bec0ec
Samantha <samanthabarrueta@gmail.com>
2020-06-07 12:55:07 -0500

    questions and answers

--- Develop/index.js
+++ Develop/index.js
@@ -1,12 +1,103 @@
-const questions = [
+const inquirer = require('inquirer')
 
-];
+inquirer.prompt([
+    {
+        type:'input',
+        name: 'title',
+        message: 'What is the title of your project?'
+    },
+    {
+        type:'input',
+        name: 'description',
+        message: 'Give a brief description of your project:'
+    },
+    {
+        type:'input',
+        name: 'tableOfContents',
+        message: 'Add a table of contents:'
+    },
+    {
+        type:'input',
+        name: 'installation',
+        message: 'Installation guidelines and requirements for your project:'
+    },
+    {
+        type:'input',
+        name: 'usage',
+        message: 'Describe the usage of your project:'
+    },
+    {
+        type:'input',
+        name: 'license',
+        message: 'How is your project licensed?'
+    },
+    {
+        type:'input',
+        name: 'contributing',
+        message: 'Contribution requirements for your project:'
+    },
+    {
+        type:'input',
+        name: 'tests',
+        message: 'Explain how to run tests for your project:'
+    },
+    {
+        type:'input',
+        name: 'github',
+        message: 'What is your github username?'
+    }
 
-function writeToFile(fileName, data) {
-}
+])
+.then((answer) => {
+    const readmeGen = `
+# ${answer.title}
 
-function init() {
+# Description:
+${answer.description}
 
-}
+# Table of Contents:
+${answer.description}
+${answer.installation}
+${answer.usage}
+${answer.license}
+${answer.contributing}
+${answer.tests}
+${answer.github}
 
-init();
+# Installation:
+${answer.installation}
+
+# Usage
+${answer.usage}
+
+# License
+${answer.license}
+
+# Contributing:
+${answer.contributing}
+
+# Tests:
+${answer.tests}
+
+# Github: 
+${answer.github}
+    `
+})
+
+ 
+
+
+
+
+// const questions = [
+
+// ];
+
+// function writeToFile(fileName, data) {
+// }
+
+// function init() {
+
+// }
+
+// init();
